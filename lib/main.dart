54b0efdb0ad797ccca795848449690304cd8d741
Saher <saherisrar320@gmail.com>
2026-04-26 23:02:27 +0500

    main file

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'screens/common/splash_screen.dart';
+import 'utils/app_colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HostelHub',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+        scaffoldBackgroundColor: AppColors.background,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
